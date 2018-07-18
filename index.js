7cf8aa80252c659c6a6fa03da22041b43cc916a3
Evan Solomon <evanlsolomon@gmail.com>
2018-07-18 23:55:24 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
 // Write your code in this file!
+scuberGreetingForFeet(rideDistance){
+  if (rideDistance < 400){
+    return 'This one is on me!'
+  } 
+  else if (rideDistance > 2000 && rideDistance < 2501){
+    return'I will gladly take your thirty bucks.';
+  }   
+  else if (rideDistance > 2501){
+   return 'No can do.';
+  } 
+  else {
+    return`Your ride is ${rideDistance} feet. Hop in!`;
+  }
+}
+
+ternaryCheckCity (city){
+  if(city === 'NYC'){
+    return 'Ok, sounds good.';
+  }
+  else{
+    return 'No go.';
+  }
+}
+
+switchOnCharmFromTip(tipQuality){
+  if (tipQuality ==='generous'){
+    return 'Thank you so much.';
+  }
+  else if(tipQuality ==='not as generous'){
+    return 'Thank you.';
+  }
+  
+  else {
+    return 'Bye.';
+  }
+  
+}
